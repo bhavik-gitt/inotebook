506b1ca02ee91b1c08d8bfa6005d976d9b4c4e43
bhavik-gitt <bhavikgithub@gmail.com>
2025-09-09 16:34:37 +0530

    Refactor About component to remove unused context and hooks

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,11 @@
-import React,{useContext, useEffect} from 'react'
-import noteContext from '../context/notes/noteContext'
+// import React,{useContext, useEffect} from 'react'
+// import noteContext from '../context/notes/noteContext'
 
 export default function About() {
-  const a = useContext(noteContext); // Using useContext hook to access the context values
-  useEffect(()=>{
-    a.update();
-    // esLint-disable-next-Line
-  })
+
   return (
     <>
-    This is About page {a.state.name}
+    This is About page 
     </>
   )
 }
