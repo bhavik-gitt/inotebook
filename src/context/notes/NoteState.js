c3a247253eb2088151c5506041c19a91f67069b4
bhavik-gitt <bhavikgithub@gmail.com>
2025-09-09 22:39:38 +0530

    Refactor NoteState to initialize state with notes array and remove unused user details

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,23 +2,38 @@ import React, { useState } from "react";
 import noteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const s1 = {
-    name: "Bhavik",
-    class: "BTech"
-  };
-  const [state,setState] = useState(s1);
-  const update = () =>
-  {
-    setTimeout(()=>
+  const notesInitial = [
     {
-        setState({
-            name : "CodeWithBhavik",
-            class : "BTect IT"
-        })
-    },1000); 
-  }
+        "_id": "68bd43af162dab6a00c70ef3",
+        "user": "68bbef57055caca971b980a7",
+        "title": "My Tittle",
+        "description": "My Description",
+        "tag": "Personal",
+        "date": "2025-09-07T08:34:55.833Z",
+        "__v": 0
+    },
+    {
+        "_id": "68bdca80ccd580eca7d5f98f",
+        "user": "68bbef57055caca971b980a7",
+        "title": "New Note2",
+        "description": "New Note description2",
+        "tag": "Personal",
+        "date": "2025-09-07T18:10:08.348Z",
+        "__v": 0
+    },
+    {
+        "_id": "68bdcb799ac39eaedc4aae81",
+        "user": "68bbef57055caca971b980a7",
+        "title": "Finding",
+        "description": "Finding Someone 💕",
+        "tag": "Personal",
+        "date": "2025-09-07T18:14:17.524Z",
+        "__v": 0
+    }
+]
+const [state, setState] = useState(notesInitial);
   return (
-    <noteContext.Provider value={{state,update}}>
+    <noteContext.Provider value={{state,setState}}>
       {props.children}
     </noteContext.Provider>
   );
