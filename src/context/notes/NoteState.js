51428e589d4ec4d3f16ce7d753bbe8d4dfdc50b6
bhavik-gitt <bhavikgithub@gmail.com>
2025-09-10 23:58:52 +0530

    Refactor NoteState to improve state management and enhance addNote functionality

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -4,36 +4,58 @@ import noteContext from "./noteContext";
 const NoteState = (props) => {
   const notesInitial = [
     {
-        "_id": "68bd43af162dab6a00c70ef3",
-        "user": "68bbef57055caca971b980a7",
-        "title": "My Tittle",
-        "description": "My Description",
-        "tag": "Personal",
-        "date": "2025-09-07T08:34:55.833Z",
-        "__v": 0
+      _id: "68bd43af162dab6a00c70ef3",
+      user: "68bbef57055caca971b980a7",
+      title: "My Tittle",
+      description: "My Description",
+      tag: "Personal",
+      date: "2025-09-07T08:34:55.833Z",
+      __v: 0,
     },
     {
-        "_id": "68bdca80ccd580eca7d5f98f",
-        "user": "68bbef57055caca971b980a7",
-        "title": "New Note2",
-        "description": "New Note description2",
-        "tag": "Personal",
-        "date": "2025-09-07T18:10:08.348Z",
-        "__v": 0
+      _id: "68bdca80ccd580eca7d5f98f",
+      user: "68bbef57055caca971b980a7",
+      title: "New Note2",
+      description: "New Note description2",
+      tag: "Personal",
+      date: "2025-09-07T18:10:08.348Z",
+      __v: 0,
     },
     {
-        "_id": "68bdcb799ac39eaedc4aae81",
-        "user": "68bbef57055caca971b980a7",
-        "title": "Finding",
-        "description": "Finding Someone 💕",
-        "tag": "Personal",
-        "date": "2025-09-07T18:14:17.524Z",
-        "__v": 0
-    }
-]
-const [state, setState] = useState(notesInitial);
+      _id: "68bdcb799ac39eaedc4aae81",
+      user: "68bbef57055caca971b980a7",
+      title: "Finding",
+      description: "Finding Someone 💕",
+      tag: "Personal",
+      date: "2025-09-07T18:14:17.524Z",
+      __v: 0,
+    },
+  ];
+  const [notes, setNotes] = useState(notesInitial);
+
+  // Add a note
+  const addNote = (title, description, tag) => {
+    //Todo api call
+    console.log("addding a new note");
+    const note = {
+      _id: "68bdcb799ac39eaedc4aae81",
+      user: "68bbef57055caca971b980a7",
+      title: "Finding",
+      description: "Finding Someone 💕 [Added]",
+      tag: "Personal",
+      date: "2025-09-07T18:14:17.524Z",
+      __v: 0,
+    }; 
+    setNotes(notes.concat(note));
+  };
+  // Delete a note
+  const deleteNote = () => {};
+
+  //Edit a note
+  const editNote = () => {};
+
   return (
-    <noteContext.Provider value={{state,setState}}>
+    <noteContext.Provider value={{ notes, setNotes,addNote, deleteNote, editNote }}>
       {props.children}
     </noteContext.Provider>
   );
