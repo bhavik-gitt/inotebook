19fd122dff97d7f49b7d74171e634007e086fb38
bhavik-gitt <bhavikgithub@gmail.com>
2026-06-03 08:07:01 +0530

    Enhance About component with structured content and improved layout

--- src/components/About.js
+++ src/components/About.js
@@ -2,5 +2,19 @@
 // import noteContext from '../context/notes/noteContext'
 
 export default function About() {
-  return <>This is About page</>;
+  return <>
+     <div className="container my-5 mx-5">
+      <h1>This is about page
+      </h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, ipsam aperiam vitae obcaecati doloribus consequatur blanditiis accusamus. Vero illo, sit eligendi et, sapiente aspernatur nihil doloremque itaque a asperiores expedita.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, ipsam aperiam vitae obcaecati doloribus consequatur blanditiis accusamus. Vero illo, sit eligendi et, sapiente aspernatur nihil doloremque itaque a asperiores expedita.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, ipsam aperiam vitae obcaecati doloribus consequatur blanditiis accusamus. Vero illo, sit eligendi et, sapiente aspernatur nihil doloremque itaque a asperiores expedita.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, ipsam aperiam vitae obcaecati doloribus consequatur blanditiis accusamus. Vero illo, sit eligendi et, sapiente aspernatur nihil doloremque itaque a asperiores expedita.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, ipsam aperiam vitae obcaecati doloribus consequatur blanditiis accusamus. Vero illo, sit eligendi et, sapiente aspernatur nihil doloremque itaque a asperiores expedita.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, ipsam aperiam vitae obcaecati doloribus consequatur blanditiis accusamus. Vero illo, sit eligendi et, sapiente aspernatur nihil doloremque itaque a asperiores expedita.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, ipsam aperiam vitae obcaecati doloribus consequatur blanditiis accusamus. Vero illo, sit eligendi et, sapiente aspernatur nihil doloremque itaque a asperiores expedita.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, ipsam aperiam vitae obcaecati doloribus consequatur blanditiis accusamus. Vero illo, sit eligendi et, sapiente aspernatur nihil doloremque itaque a asperiores expedita.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, ipsam aperiam vitae obcaecati doloribus consequatur blanditiis accusamus. Vero illo, sit eligendi et, sapiente aspernatur nihil doloremque itaque a asperiores expedita.</p>
+     </div>
+  </>;
 }
